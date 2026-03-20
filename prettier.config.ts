5e955fefc268adf053803aadb9bc70f89f019803
Yuki <admin@yuki.sh>
2026-03-20 14:51:33 +0800

    chore(tooling): add lint and format configs

---
+++ prettier.config.ts
@@ -0,0 +1,11 @@
+import { type Config } from 'prettier';
+
+export default {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+} satisfies Config;
